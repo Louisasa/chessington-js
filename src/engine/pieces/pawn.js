dc837697d1ab181b886273804e27fdf0411904f2
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-22 10:35:38 +0100

    prawns can now move two spaces on first go

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,8 +1,9 @@
 import Piece from './piece';
-import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
 
 export default class Pawn extends Piece {
+
     constructor(player) {
         super(player);
     }
@@ -10,11 +11,18 @@ export default class Pawn extends Piece {
     getAvailableMoves(board) {
         const pawnPlace = board.findPiece(this);
         const outputArray = [];
-        if (pawnPlace.row < 7) {
+        if (pawnPlace.row < 7 && this.player === Player.WHITE) {
             outputArray.push(new Square(pawnPlace.row+1, pawnPlace.col));
         }
-        if (pawnPlace.row > 0) {
+        if (pawnPlace.row > 0 && this.player === Player.BLACK) {
             outputArray.push(new Square(pawnPlace.row-1, pawnPlace.col));
+        }
+        // if on specific row means it is the first move
+        if (pawnPlace.row === 1 && this.player === Player.WHITE) {
+            outputArray.push(new Square(pawnPlace.row+2, pawnPlace.col));
+        }
+        if (pawnPlace.row === 6 && this.player === Player.BLACK) {
+            outputArray.push(new Square(pawnPlace.row-2, pawnPlace.col));
         }
         return outputArray;
     }
