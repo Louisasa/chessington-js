39afa61d0e2dea44e20f0744ec2a8d2c3fd7acca
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-22 11:11:21 +0100

    bishop moves diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,39 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const bishopPlace = board.findPiece(this);
+        let bishopRow = bishopPlace.row;
+        let bishopCol = bishopPlace.col;
+        const outputArray = [];
+        // need to check there isn't a piece in the way
+        while(bishopRow+1 <= 7 && bishopCol+1 <= 7) {
+            // check piece isn't there
+            outputArray.push(new Square(bishopRow+1, bishopCol+1));
+            bishopRow++;
+            bishopCol++;
+        }
+        bishopRow = bishopPlace.row;
+        bishopCol = bishopPlace.col;
+        while (bishopRow+1 <= 7 && bishopCol-1 >= 0) {
+            outputArray.push(new Square(bishopRow+1, bishopCol-1));
+            bishopRow++;
+            bishopCol--;
+        }
+        bishopRow = bishopPlace.row;
+        bishopCol = bishopPlace.col;
+        while(bishopRow-1 >= 0 && bishopCol+1 <= 7) {
+            outputArray.push(new Square(bishopRow-1, bishopCol+1));
+            bishopRow--;
+            bishopCol++;
+        }
+        bishopRow = bishopPlace.row;
+        bishopCol = bishopPlace.col;
+        while (bishopRow-1 >= 0 && bishopCol-1 >= 0) {
+            outputArray.push(new Square(bishopRow-1, bishopCol-1));
+            bishopRow--;
+            bishopCol--;
+        }
+        console.log(outputArray);
+        return outputArray;
     }
 }
