d4b4cbdae0cfe7e3dc8c856c240e2d2311ce9f89
Louisa Nash <Louisa.Nash@softwire.com>
2019-07-22 10:25:47 +0100

    Prawns move and pass tests

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Board from '../../../src/engine/board';
+import Square from '../../../src/engine/square';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,14 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const pawnPlace = board.findPiece(this);
+        const outputArray = [];
+        if (pawnPlace.row < 7) {
+            outputArray.push(new Square(pawnPlace.row+1, pawnPlace.col));
+        }
+        if (pawnPlace.row > 0) {
+            outputArray.push(new Square(pawnPlace.row-1, pawnPlace.col));
+        }
+        return outputArray;
     }
 }
